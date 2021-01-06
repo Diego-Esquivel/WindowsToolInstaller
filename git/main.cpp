da361e4c51d6ffa60c74e2dc16867e91cb23ceb4
Diego Esquivel <mpi.diegoe@gmail.com>
2021-01-06 09:55:15 -0600

    Update files

--- git/main.cpp
+++ git/main.cpp
@@ -3,32 +3,50 @@
 using std::string;
 
 //function prototypes
+int configgitglobaldata(char[], string);
+int configgitglobaldata(string,string);
 string configgitusernamestring(char*);
 
+//global variables
+char username[30];
+string email = "";
+
 int main () {
-    char username[30];
-    string email = "";
+    
     std::cout << "Git username: ";
     std::cin >> username;
     std::cin.clear();
-    std::cin.ignore('\n', 128);
+    std::cin.ignore(128, '\n');
     std::cout << "Git email: ";
     std::cin >> email;
     std::cin.clear();
-    std::cin.ignore('\n', 128);
-    string gitusernamestring = "";
-    string gituseremailstring = "";
+    std::cin.ignore(128, '\n');
+    
     if(system("git version")) {
-        system("gitinstall");
-        gitusernamestring = configgitusernamestring(username);
-        system(gitusernamestring.c_str());
-        gituseremailstring = "git config --global user.email " + email;
-        system(gituseremailstring.c_str());
-        system("git init");
+        std::system("gitinstall");
     }
     else 
         std::cout << "Git already installed. Congratulations!";
+    configgitglobaldata(username,email);
+    std::system("git init");
     std::system("PAUSE");
+    return 1;
+}
+
+int configgitglobaldata(char aname[], string aemail) {
+    string gitusernamestring = "";
+    gitusernamestring = configgitusernamestring(aname);
+    configgitglobaldata(gitusernamestring,aemail);
+    return 0;
+}
+
+int configgitglobaldata(string aname, string aemail) {
+    string gitusernamestring = "";
+    string gituseremailstring = "";
+    gitusernamestring = aname;
+    system(gitusernamestring.c_str());
+    gituseremailstring = "git config --global user.email " + aemail;
+    system(gituseremailstring.c_str());
     return 0;
 }
 
