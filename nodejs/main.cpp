28333e36892eb09a9f912e47cd497f63a4f56d0b
Diego Esquivel <mpi.diegoe@gmail.com>
2021-04-14 20:13:47 -0500

    remove files

--- nodejs/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-using std::string;
-
-int main () {
-    string nodejsrequest = "nodejsinstall";
-    system(nodejsrequest.c_str());
-    return 1;
-}
--- nodejs/nodejsinstall.bat
+++
@@ -1,3 +0,0 @@
-curl https://nodejs.org/dist/v14.16.0/node-v14.16.0-x64.msi --output nodejsbashinstall.msi
-nodejsbashinstall.msi
-npm i puppeteer
